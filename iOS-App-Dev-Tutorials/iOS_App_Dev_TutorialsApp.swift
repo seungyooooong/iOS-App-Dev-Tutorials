066dbd48abff01e5ffa23253659985535bab64fc
seungyooooong <chadange@naver.com>
2022-07-12 18:44:19 +0900

    Initial Commit

---
+++ iOS-App-Dev-Tutorials/iOS_App_Dev_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_App_Dev_TutorialsApp.swift
+//  iOS-App-Dev-Tutorials
+//
+//  Created by 최승용 on 2022/07/12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_App_Dev_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
