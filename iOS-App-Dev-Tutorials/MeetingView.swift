b859f1a0c1ab24c21ae1f64e58886f0af1a8c3d0
seungyooooong <chadange@naver.com>
2022-07-13 21:31:26 +0900

    Supplement Accessibility Data

--- iOS-App-Dev-Tutorials/MeetingView.swift
+++ iOS-App-Dev-Tutorials/MeetingView.swift
@@ -22,6 +22,9 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
             Circle().strokeBorder(lineWidth: 24)
             HStack {
                 Text("Speaker 1 of 3")
@@ -29,8 +32,10 @@ struct MeetingView: View {
                 Button(action: {}) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
-        }.padding()
+        }
+        .padding()
     }
 }
 
