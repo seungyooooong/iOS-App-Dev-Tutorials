49d9724e364da7b5fde116a50f6fea41f133c6c5
seungyooooong <chadange@naver.com>
2022-07-17 23:45:00 +0900

    remove empty space

--- iOS-App-Dev-Tutorials/Models/Theme.swift
+++ iOS-App-Dev-Tutorials/Models/Theme.swift
@@ -29,7 +29,7 @@ enum Theme: String {
         switch self {
             case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
             case .indigo, .magenta, .navy, .oxblood, .purple: return .white
-            }
+        }
     }
     var mainColor: Color {
         Color(rawValue)
