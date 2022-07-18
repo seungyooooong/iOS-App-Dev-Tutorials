5f97c6dcd4414d3c3ee2dba5ab0233a6d29e177e
seungyooooong <chadange@naver.com>
2022-07-18 17:02:06 +0900

    Display a List of Daily Scrums

---
+++ iOS-App-Dev-Tutorials/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  iOS-App-Dev-Tutorials
+//
+//  Created by 최승용 on 2022/07/18.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
