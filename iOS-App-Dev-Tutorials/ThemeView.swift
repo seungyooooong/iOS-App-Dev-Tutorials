9410a1729d03dac5894ec6d86da83bed4df81609
seungyooooong <chadange@naver.com>
2022-07-25 23:39:39 +0900

    Add a Theme View

---
+++ iOS-App-Dev-Tutorials/ThemeView.swift
@@ -0,0 +1,29 @@
+//
+//  ThemeView.swift
+//  iOS-App-Dev-Tutorials
+//
+//  Created by 최승용 on 2022/07/25.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 4)
+                .fill(theme.mainColor)
+            Label(theme.name, systemImage: "paintpalette")
+                .padding(4)
+        }
+        .foregroundColor(theme.accentColor)
+        .fixedSize(horizontal: false, vertical: true)
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .buttercup)
+    }
+}
