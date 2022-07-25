6a37d7b7ca807fe3258ef2c7e1d98b40f94f107a
seungyooooong <chadange@naver.com>
2022-07-26 00:00:56 +0900

    Add a Theme Picker

---
+++ iOS-App-Dev-Tutorials/ThemePicker.swift
@@ -0,0 +1,27 @@
+//
+//  ThemePicker.swift
+//  iOS-App-Dev-Tutorials
+//
+//  Created by 최승용 on 2022/07/25.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
