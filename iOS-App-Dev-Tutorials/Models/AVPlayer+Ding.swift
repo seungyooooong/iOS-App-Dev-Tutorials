f4e16cad67bd2ba57cc029c330d6d9b203657a39
seungyooooong <chadange@naver.com>
2022-08-05 15:48:57 +0900

    copy AVPlayer+Ding.swift from starter project

---
+++ iOS-App-Dev-Tutorials/Models/AVPlayer+Ding.swift
@@ -0,0 +1,20 @@
+//
+//  AVPlayer+Ding.swift
+//  iOS-App-Dev-Tutorials
+//
+//  Created by 최승용 on 2022/08/05.
+//
+
+/*
+See LICENSE folder for this sample’s licensing information.
+*/
+
+import Foundation
+import AVFoundation
+
+extension AVPlayer {
+    static let sharedDingPlayer: AVPlayer = {
+        guard let url = Bundle.main.url(forResource: "ding", withExtension: "wav") else { fatalError("Failed to find sound file.") }
+        return AVPlayer(url: url)
+    }()
+}
