113e9e4cecade2f9854989122226ef5ddce2e2cb
seungyooooong <chadange@naver.com>
2022-08-07 15:23:09 +0900

    Add Scrum History

---
+++ iOS-App-Dev-Tutorials/Models/History.swift
@@ -0,0 +1,22 @@
+//
+//  History.swift
+//  iOS-App-Dev-Tutorials
+//
+//  Created by 최승용 on 2022/08/07.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var lengthInMinutes: Int
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+    }
+}
