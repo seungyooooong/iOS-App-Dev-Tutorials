6a8b72ce015e39913d6e2e58b9948ce906324aa3
seungyooooong <chadange@naver.com>
2022-08-08 14:41:39 +0900

    Create a Data Store

---
+++ iOS-App-Dev-Tutorials/Models/ScrumStore.swift
@@ -0,0 +1,21 @@
+//
+//  ScrumStore.swift
+//  iOS-App-Dev-Tutorials
+//
+//  Created by 최승용 on 2022/08/08.
+//
+
+import Foundation
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
+    }
+}
