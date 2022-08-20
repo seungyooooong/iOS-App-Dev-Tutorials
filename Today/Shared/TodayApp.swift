76df60b22c1620c26902babbd319c02db02e1609
seungyooooong <chadange@naver.com>
2022-08-20 23:58:32 +0900

    Create a Project

---
+++ Today/Shared/TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayApp.swift
+//  Shared
+//
+//  Created by 최승용 on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
