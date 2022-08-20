acada6957ab1b0af8a5ccbbe52f479174bf8fffe
seungyooooong <chadange@naver.com>
2022-08-21 00:01:11 +0900

    Remove previous Project and Recreate a Project for iOS

---
+++ Today/Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by 최승용 on 2022/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
