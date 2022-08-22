ffc7575bfc8faada8a184db578d146fef11c8340
seungyooooong <chadange@naver.com>
2022-08-22 19:47:35 +0900

    Configure the Collection as a List

---
+++ Today/Today/ReminderListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by 최승용 on 2022/08/21.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+    }
+
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+}
+
