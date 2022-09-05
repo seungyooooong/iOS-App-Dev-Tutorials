90175df80213af9343dd0bc9c99821f86c0da474
seungyooooong <chadange@naver.com>
2022-09-05 11:04:21 +0900

    Conform to the content view protocol

---
+++ Today/Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by 최승용 on 2022/09/05.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> self {
+        return self
+    }
+}
