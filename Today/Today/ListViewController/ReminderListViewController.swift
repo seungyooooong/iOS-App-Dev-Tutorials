722a9da0d731b829c9b78c549d9aed30ed78ff82
seungyooooong <chadange@naver.com>
2022-09-24 16:20:20 +0900

    Display reminders

--- Today/Today/ListViewController/ReminderListViewController.swift
+++ Today/Today/ListViewController/ReminderListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController {
     var dataSource: DataSource!
-    var reminders: [Reminder] = Reminder.sampleData
+    var reminders: [Reminder] = []
     var filteredReminders: [Reminder] {
         return reminders.filter { listStyle.shouldInclude(date: $0.dueDate) }.sorted { $0.dueDate < $1.dueDate }
     }
@@ -57,6 +57,8 @@ class ReminderListViewController: UICollectionViewController {
         updateSnapshot()
         
         collectionView.dataSource = dataSource
+        
+        prepareReminderStore()
     }
     
     override func viewWillAppear(_ animated: Bool) {
